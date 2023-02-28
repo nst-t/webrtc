62420e894ceded29b3acda7f419a3b5d62c1b84d
nst-t <tyler@nstrumenta.com>
2023-02-27 20:11:37 -0800

    cleans up selector

--- src/components/selector.tsx
+++ src/components/selector.tsx
@@ -3,13 +3,11 @@ import { FC, useState } from 'react';
 
 export const Selector: FC<{
   button: string;
-  onClick: (res: boolean) => void;
+  onClick: () => void;
 }> = ({ button, onClick }) => {
-  const [value, setValue] = useState('false');
-
   return (
     <Stack direction="row" padding={1}>
-      <Button onClick={() => onClick(value === 'true' ? true : false)}>{button}</Button>
+      <Button onClick={onClick}>{button}</Button>
     </Stack>
   );
 };
